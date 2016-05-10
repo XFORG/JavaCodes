800a6645b7703bce51b4805f0926f50b0da6bed4
XFORG <18645043633@163.com>
2016-05-10 13:52:24 +0800

    OK

---
+++ src/com/xforg/concurrency/CountDownLatchDemo.java
@@ -0,0 +1,68 @@
+package com.xforg.concurrency;//: concurrency/CountDownLatchDemo.java
+import java.util.concurrent.*;
+import java.util.*;
+import static com.xforg.utils.Print.print;
+/*CountDownLatch用来同步一个和多个任务，强制它们等待其他任务执行的一组操作完成*/
+
+// Performs some portion of a task:
+class TaskPortion implements Runnable {
+  private static int counter = 0;
+  private final int id = counter++;
+  private static Random rand = new Random(47);
+  private final CountDownLatch latch;
+  TaskPortion(CountDownLatch latch) {
+    this.latch = latch;
+  }
+  public void run() {
+    try {
+      doWork();
+      latch.countDown();/*锁存器计数减一*/
+    } catch(InterruptedException ex) {
+      // Acceptable way to exit
+    }
+  }
+  public void doWork() throws InterruptedException {
+    TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+    print(this + "doWork(): completed");
+  }
+  public String toString() {
+    return String.format("%1$-3d ", id);
+  }
+}
+
+// Waits on the CountDownLatch:
+class WaitingTask implements Runnable {
+  private static int counter = 0;
+  private final int id = counter++;
+  private final CountDownLatch latch;
+  WaitingTask(CountDownLatch latch) {
+    this.latch = latch;
+  }
+  public void run() {
+    try {
+      latch.await();/*任何在Latch对象上调用wait（）方法都将阻塞，直至这个计数值到达0，其他任务在结束工作时，可以在该对象上调用countDown
+      来减小这个数值*/
+      print("Latch barrier passed for " + this);
+    } catch(InterruptedException ex) {
+      print(this + " interrupted");
+    }
+  }
+  public String toString() {
+    return String.format("WaitingTask %1$-3d ", id);
+  }
+}
+
+public class CountDownLatchDemo {
+  static final int SIZE = 100;
+  public static void main(String[] args) throws Exception {
+    ExecutorService exec = Executors.newCachedThreadPool();
+    // All must share a single CountDownLatch object:
+    CountDownLatch latch = new CountDownLatch(SIZE);
+    for(int i = 0; i < 10; i++)
+      exec.execute(new WaitingTask(latch));
+    for(int i = 0; i < SIZE; i++)
+      exec.execute(new TaskPortion(latch));
+    print("Launched all tasks");
+    exec.shutdown();
+  }
+}
